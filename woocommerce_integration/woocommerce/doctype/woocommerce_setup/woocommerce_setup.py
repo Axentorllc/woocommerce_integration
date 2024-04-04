9606e144bccd78b432db485d0fc7b14cbd23a9c2
marination <maricadsouza221197@gmail.com>
2024-04-04 17:03:36 +0200

    refactor: Simplify job scheduling + separate sync methods

--- woocommerce_integration/woocommerce/doctype/woocommerce_setup/woocommerce_setup.py
+++ woocommerce_integration/woocommerce/doctype/woocommerce_setup/woocommerce_setup.py
@@ -9,97 +9,129 @@ from frappe.model.document import Document
 from woocommerce_integration.webhooks import ACTION_MAP
 from woocommerce_integration.general_utils import get_woocommerce_setup
 
-STOCK_UPDATE_METHODS = frappe._dict(
-	event_based="On Stock Update",
-	time_based="In Certain Time Intervals"
-)
-
 
 class WooCommerceSetup(Document):
-	def onload(self):
-		series = frappe.get_meta("Sales Order").get_options("naming_series") or "SO-WOO-"
-		self.set_onload("sales_order_series", series)
+    def onload(self):
+        series = (
+            frappe.get_meta("Sales Order").get_options("naming_series") or "SO-WOO-"
+        )
+        self.set_onload("sales_order_series", series)
 
-	def before_validate(self):
-		self.set_webhook_urls()
+    def before_validate(self):
+        self.set_webhook_urls()
 
-	def validate(self):
-		self.validate_interval()
+    def validate(self):
+        self.validate_interval()
+        self.setup_scheduled_job()
 
-	def on_update(self):
-		self.create_scheduled_job()
+    def validate_interval(self):
+        if (self.enable_stock_sync and self.stock_sync_interval > 60) or (
+            self.enable_order_sync
+            and self.order_sync_frequency == "Minutes"
+            and self.order_sync_interval > 60
+        ):
+            frappe.throw(_("Interval in minutes cannot be greater than 60 minutes."))
 
-	def validate_interval(self):
-		if (
-			self.enable_stock_sync
-			and self.stock_update_method == STOCK_UPDATE_METHODS.time_based
-			and self.interval_in_minutes > 60
-		):
-			frappe.throw(
-				_("Interval in minutes cannot be greater than 60 minutes.")
-			)
+    @frappe.whitelist()
+    def generate_secret(self):
+        woocommerce_setup = get_woocommerce_setup()
+        woocommerce_setup.webhook_secret = frappe.generate_hash()
+        woocommerce_setup.save()
 
-	@frappe.whitelist()
-	def generate_secret(self):
-		woocommerce_setup = get_woocommerce_setup()
-		woocommerce_setup.webhook_secret = frappe.generate_hash()
-		woocommerce_setup.save()
+    def set_webhook_urls(self):
+        """Set the webhook URLs for the WooCommerce actions."""
+        if self.webhook_endpoints:
+            return
 
-	def set_webhook_urls(self):
-		"""Set the webhook URLs for the WooCommerce actions."""
-		if self.webhook_endpoints:
-			return
+        try:
+            url = frappe.request.url
+        except Exception:
+            url = "http://localhost:8000"
 
-		try:
-			url = frappe.request.url
-		except Exception:
-			url = "http://localhost:8000"
+        server_url = "{uri.scheme}://{uri.netloc}".format(uri=urlparse(url))
+        path_to_webhooks = "/api/method/woocommerce_integration.webhooks"
+        endpoints = [
+            f"{action}: {server_url + path_to_webhooks + '.' + method}"
+            for action, method in ACTION_MAP.items()
+        ]
 
-		server_url = "{uri.scheme}://{uri.netloc}".format(uri=urlparse(url))
-		path_to_webhooks = "/api/method/woocommerce_integration.webhooks"
-		endpoints = [
-			f"{action}: {server_url + path_to_webhooks + '.' + method}"
-			for action, method in ACTION_MAP.items()
-		]
+        self.webhook_endpoints = "\n".join(endpoints)
 
-		self.webhook_endpoints = "\n".join(endpoints)
+    def setup_scheduled_job(self):
+        """Start/Stop/Edit a scheduled server script for stock/order sync."""
+        self.create_update_job("stock")
+        self.create_update_job("order")
 
-	def create_scheduled_job(self):
-		"""Create a scheduled job for the stock sync."""
-		if not self.enable_stock_sync or (
-			self.stock_update_method != STOCK_UPDATE_METHODS.time_based
-		) or not self.interval_in_minutes:
-			if self.scheduled_job:
-				# Disable the scheduled job
-				self.disable_job(disabled=True)
+    def create_update_job(self, job_type: str):
+        """Create/update/enable/disable a scheduled server script for stock/order sync."""
+        sync_switch = f"enable_{job_type}_sync"
+        interval_field = f"{job_type}_sync_interval"
+        script_field = f"{job_type}_server_script"
+        frequency_field = f"{job_type}_sync_frequency" if job_type == "order" else None
+        if script_name := self.get(script_field):
+            # Update the existing script as per the new interval
+            server_script = frappe.get_doc("Server Script", script_name)
+        else:
+            server_script = frappe.get_doc(
+                dict(
+                    doctype="Server Script",
+                    __newname=f"Batch Sync WooCommerce {job_type.title()}",
+                    script_type="Scheduler Event",
+                    module="WooCommerce",
+                    script=f"frappe.call('woocommerce_integration.woocommerce.sync_utils.batch_sync_{job_type}')",
+                )
+            )
 
-			return
+        server_script.disabled = not self.get(sync_switch)
+        frequency = self.get(frequency_field) if frequency_field else "Minutes"
+        # Set the frequency in the script
+        server_script.update(
+            self.get_script_frequency(frequency, self.get(interval_field))
+        )
+        server_script.save()
+        setattr(self, script_field, server_script.name)
 
-		if not self.has_value_changed("interval_in_minutes"):
-			# No change in interval, no need to create/update a scheduled job
-			# Just enable the job if it was disabled
-			self.disable_job(disabled=False)
-			return
+    def disable_script(self, disabled: bool, script: str):
+        """Enable or disable the scheduled job."""
+        server_script = frappe.get_doc("Server Script", script)
+        server_script.disabled = disabled
+        server_script.save()
 
-		if self.scheduled_job:
-			job = frappe.get_doc("Scheduled Job Type", self.scheduled_job)
-			job.cron_format = f"0/{self.interval_in_minutes} * * * *"
-			job.stopped = 0
-		else:
-			job = frappe.get_doc(
-				dict(
-					doctype="Scheduled Job Type",
-					method="woocommerce_integration.stock_sync_utils.batch_update_stock",
-					frequency="Cron",
-					cron_format=f"0/{self.interval_in_minutes} * * * *",
-				)
-			)
+    def get_script_frequency(self, frequency: str, value: str) -> str:
+        if frequency == "Minutes":
+            return frappe._dict(
+                event_frequency="Cron",
+                cron_format=f"0/{value} * * * *",
+            )
+        return frappe._dict(event_frequency=frequency)
 
-		job.save()
-		self.db_set("scheduled_job", job.name)
 
-	def disable_job(self, disabled: bool):
-		"""Enable or disable the scheduled job."""
-		job = frappe.get_doc("Scheduled Job Type", self.scheduled_job)
-		job.stopped = disabled
-		job.save()
+@frappe.whitelist()
+def sync_stock() -> None:
+    """Sync stock from ERPNext to WooCommerce."""
+    frappe.enqueue(
+        method="woocommerce_integration.woocommerce.sync_utils.batch_sync_stock",
+        now=frappe.conf.developer_mode,
+    )
+
+    frappe.msgprint(
+        _("Stock is being synced from ERPNext to WooCommerce in the background."),
+        alert=True,
+        indicator="blue",
+    )
+
+
+@frappe.whitelist()
+def sync_orders() -> None:
+    """Sync orders from WooCommerce to ERPNext."""
+    frappe.enqueue(
+        method="woocommerce_integration.woocommerce.sync_utils.batch_sync_order",
+        now=frappe.conf.developer_mode,
+        queue="long",
+    )
+
+    frappe.msgprint(
+        _("Orders are being synced from WooCommerce to ERPNext in the background."),
+        alert=True,
+        indicator="blue",
+    )
---
+++ woocommerce_integration/woocommerce/sync_utils.py
@@ -0,0 +1,91 @@
+import frappe
+from frappe.utils import cint, get_datetime
+
+from woocommerce_integration.general_utils import get_woocommerce_setup
+from woocommerce_integration.order_creation_utils import create_sales_order
+from woocommerce_integration.woocommerce_connector import WooCommerceConnector
+
+
+@frappe.whitelist()
+def batch_sync_stock():
+    """
+    Flow: From ERPNext to WooCommerce.
+    Called by the scheduler. Batch update items from all recent stock updates.
+    """
+    setup = get_woocommerce_setup()
+    setup.check_permission("write")
+    if not setup.enable_stock_sync:
+        return
+
+    filters = {"warehouse": setup.warehouse}
+    if setup.last_stock_sync:
+        filters["modified"] = (">=", setup.last_stock_sync)
+
+    # Get all recent Bins
+    data = {"update": []}
+    for row in frappe.get_all(
+        "Bin", filters=filters, fields=["item_code", "actual_qty"]
+    ):
+        if product_id := frappe.db.get_value(
+            "Item", row.item_code, "woocomm_product_id"
+        ):
+            data["update"].append(
+                {
+                    "id": product_id,
+                    "stock_quantity": cint(row.actual_qty),
+                    "manage_stock": True,
+                }
+            )
+
+    # Update stock in WooCommerce
+    if data["update"]:
+        connector = WooCommerceConnector(setup)
+        connector.batch_update_products(data)
+        frappe.db.set_value(
+            "WooCommerce Setup", None, "last_stock_sync", frappe.utils.now()
+        )
+
+
+@frappe.whitelist()
+def batch_sync_order():
+    """Batch sync orders from WooCommerce to ERPNext."""
+    setup = get_woocommerce_setup()
+    setup.check_permission("write")
+
+    if not setup.enable_order_sync:
+        return
+
+    for order in get_woocommerce_orders():
+        create_sales_order(order, setup)
+
+    frappe.db.set_value(
+        "WooCommerce Setup", None, "last_order_sync", frappe.utils.now()
+    )
+
+
+def get_woocommerce_orders():
+    """Get all the new orders from WooCommerce."""
+    setup = get_woocommerce_setup()
+    woocommerce = WooCommerceConnector(setup)
+    last_sync_datetime = (
+        get_datetime(setup.last_order_sync).isoformat()
+        if setup.last_order_sync
+        else None
+    )
+    per_page = cint(setup.order_per_page) or 10
+    orders = []
+
+    order_response = woocommerce.get_orders(
+        modified_after=last_sync_datetime, per_page=per_page
+    )
+
+    orders.extend(order_response.json())
+    pages = cint(order_response.headers.get("X-WP-TotalPages") or 1)
+
+    for page_idx in range(1, pages):
+        order_response = woocommerce.get_orders(
+            page=page_idx + 1, per_page=per_page, modified_after=last_sync_datetime
+        )
+        orders.extend(order_response.json())
+
+    return orders
